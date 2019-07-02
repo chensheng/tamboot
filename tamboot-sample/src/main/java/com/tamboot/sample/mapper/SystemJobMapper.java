c488cace27339f01670aa019eb98906266eeec78
chensheng <742611497@qq.com>
2019-07-02 23:55:45 +0800

    remove tamboot-sample module

--- tamboot-sample/src/main/java/com/tamboot/sample/mapper/SystemJobMapper.java
+++
@@ -1,9 +0,0 @@
-package com.tamboot.sample.mapper;
-
-import com.tamboot.sample.model.SystemJobModel;
-
-import java.util.List;
-
-public interface SystemJobMapper {
-    List<SystemJobModel> selectAll();
-}
